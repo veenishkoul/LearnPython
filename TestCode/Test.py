da0f79c464a06db561cbeaa6361cf11e46a31168
veenishkoul <veenishkoul@gmail.com>
2023-11-20 09:52:07 +0530

    test lines

---
+++ TestCode/Test.py
@@ -0,0 +1,9 @@
+import sys
+n = len(sys.argv)
+print("Enter the arguments :", n)
+print("Name of the python script :", sys.argv[0])
+
+x = 0
+while x < 10:
+        print("Hello world : ", x + 1)
+        x = x + 1
