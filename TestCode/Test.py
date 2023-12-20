cfeaa7791fcfa3717ae55db07d1a171b35b05262
veenishkoul <veenishkoul@gmail.com>
2023-12-20 13:32:33 -0500

    add comments

--- TestCode/Test.py
+++ TestCode/Test.py
@@ -1,8 +1,9 @@
+#Input the argumnets while running the code 
 import sys
 n = len(sys.argv)
 print("Enter the arguments :", n)
 print("Name of the python script :", sys.argv[0])
-
+#This code generates recursive numbers in next lines 
 x = 0
 while x < 10:
         print("Hello world : ", x + 1)
